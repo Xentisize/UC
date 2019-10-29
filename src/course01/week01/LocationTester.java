80fba68f9a7d69473b78733b93336c3de6297472
Xens Chan <xentisize@gmail.com>
2019-10-30 02:00:55 +0800

    Course 1 Week 1 Completed

---
+++ src/course01/week01/LocationTester.java
@@ -0,0 +1,12 @@
+package course01.week01;
+
+public class LocationTester {
+    public static void main(String[] args) {
+        SimpleLocation ucsd = new SimpleLocation(32.9, -117.2);
+        SimpleLocation lima = new SimpleLocation(-12.0, -77.0);
+
+        System.out.println(ucsd.getLatitude());
+
+        System.out.println(ucsd.distance(lima));
+    }
+}
